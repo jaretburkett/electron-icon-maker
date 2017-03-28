fe1d236f7731753156a1b355626850f4865af28b
jasonmerry <github@jasonmerry.co.uk>
2017-03-28 15:03:42 +0100

    Correct .png naming for Electron
    The .png file names were not as per the Electron docs so changed them to be 'YYxYY.png'
    
    This also added a missing '64x64' size icon to the .icns file :)

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function createPNGs(position) {
 }
 
 function createPNG(size, callback) {
-    var fileName = size.toString() + '.png';
+    var fileName = size.toString() + 'x' + size.toString() + '.png';
 
     // make dir if does not exist
     if (!fs.existsSync(output)) {
